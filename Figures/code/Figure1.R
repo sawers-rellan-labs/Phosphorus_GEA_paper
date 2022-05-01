ae5ee281f3f964eacd8b90bce480c1bc8e0008b6
faustovrz <faustovrz@gmail.com>
2022-05-01 19:09:54 -0400

    Added UN region name standards.

--- Figures/code/Figure1.R
+++ Figures/code/Figure1.R
@@ -25,7 +25,8 @@ data_dir<- file.path("..","data")
 # id_map <- read.table(file = id_map_file, header = TRUE, quote = "", sep = "\t")
 # GWAS_id <- read.table(file = GWAS_id_file, header = FALSE, quote = "", sep ="\t") 
 
-colnames(id_map)
+# colnames(id_map)
+
 accn_csv <- file.path(data_dir, "germinate_SeeD_GWAS_GBS_4022.csv")
 id_map_csv <-file.path(data_dir, "SEED_GWAS_GID.csv")
 GWAS_id_csv <- file.path(data_dir,"GWAS_ID.csv")
@@ -53,77 +54,81 @@ geo_loc<- id_map %>%
   dplyr::arrange(sample_id) %>%
   dplyr::select(bank_number,taxa,locations_region,countries_country_code3,starts_with("locations"))
 
-# Use packages to make this.
+
 # library(geonames)
-# library(ISOcodes)
-install.packages("geonames")
-install.packages("ISOcodes")
+# 
+# options(geonamesUsername="faustovrz")
+# 
+# get_country_geonames <- function(x,y){ 
+#   lapply(1:(length(x)), FUN = function(idx){
+#     lon = x[idx]
+#     lat = y[idx]
+#     if(!is.na(lon) & !is.na(lat)){
+#       geonames::GNcountryCode( lng = lon , lat = lat, radius = 10) %>%
+#         as.data.frame()
+#     } else{
+#       data.frame(languages = NA, distance = NA, 
+#            countryCode = NA, countryName = NA)
+#     }
+#   }) %>% dplyr::bind_rows()
+# }
+ISOcodes::UN_M.49_Countries
 
-library(geonames)
-
-options(geonamesUsername="faustovrz")
-
-get_country_df <- function(x,y){ 
-  lapply(1:(length(x)), FUN = function(idx){
-    lon = x[idx]
-    lat = y[idx]
-    if(!is.na(lon) & !is.na(lat)){
-      geonames::GNcountryCode( lng = lon , lat = lat, radius = 10) %>%
-        as.data.frame()
-    } else{
-      data.frame(languages = NA, distance = NA, 
-           countryCode = NA, countryName = NA)
-    }
-  }) %>% dplyr::bind_rows()
-}
-
+ISOcodes::UN_M.49_Regions
 
 library(sp)
 library(rworldmap)
 library(ISOcodes)
 
-get_country_df <- function(x,y){ 
+get_country_df <- function(x){ 
   
+  points <- x %>% 
+    dplyr::filter(!is.na(LAT) & !is.na(LON)) %>% 
+    dplyr::arrange(LON,LAT) %>%
+    dplyr::distinct()
+  nrow(x)
   countriesSP <- getMap(resolution='low')
-  
-  points <- data.frame(lon = 4, lat = 72)
-  
-  NApoint <- SpatialPoints(points,
-                           proj4string=CRS(proj4string(countriesSP))) 
-  forNA <- over(pointsSP, countriesSP)
-  forNA[1,] <- NA
 
+  pointsSP <- SpatialPoints(points, proj4string=CRS(proj4string(countriesSP))) 
+
+  country <- sp::over(pointsSP, countriesSP)
+  country$LON <- points$LON
+  country$LAT <- points$LAT
   
-  lapply(1:(length(x)), FUN = function(idx){
-    
-    if(!is.na(x) & !is.na(y)){
-      points <-data.frame( lon = x[idx], lat = y[idx])
-      pointsSP <- SpatialPoints(points,
-                               proj4string=CRS(proj4string(countriesSP))) 
-      over(pointsSP, countriesSP)
-    }
-    else{
-      forNA
-     }
-  }) %>% dplyr::bind_rows()
+  x %>% dplyr::left_join(
+    country
+  )
 }
 
 
+maize_country <- get_country_df(
+ data.frame(
+  LON = geo_loc$locations_longitude,
+  LAT = geo_loc$locations_latitude)
+)
 
+summary(maize_country)
 
-ISO_3166_1
+# UNM49 Central america
+# Standard country or area codes for statistical use (M49)
+# https://unstats.un.org/unsd/methodology/m49/
 
+region <- subset(UN_M.49_Regions, Name == "Central America")
+codes <- unlist(strsplit(region$Children, ", "))
+CA <- subset(UN_M.49_Countries, Code %in% codes)
 
+CA_maize <- subset(maize_country, ISO3.1 %in% CA$ISO_Alpha_3)
 
-maize_country <- get_country_df(x = geo_loc$locations_longitude,
-               y = geo_loc$locations_latitude)
+# UNEP GEO-3 "Meso-America"
+# Global Environment Outlook 3 Report
+# https://www.unep.org/resources/global-environment-outlook-3
 
-UNM49 <- read.csv(url("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv")) 
-colnames(UNM49)
-region <- UNM49[,c("alpha.3","region","intermediate.region")]
+MESO_maize <- subset(maize_country, GEO3 == "Meso-America")
 
-geo_loc<- geo_loc %>%
-  dplyr::left_join(region, by = c("countries_country_code3" = "alpha.3" ))
-colnames(geo_loc)
-table(geo_loc$countries_country_code3)
+summary(CA_maize$ISO3.1)
+
+summary(MESO_maize$ISO3.1)
+
+# The terms seem to behave identically in his set
+
 
